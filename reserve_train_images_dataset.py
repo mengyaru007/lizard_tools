40d35afe62f70d297c09bec97d73cd9180477b6d
mengyr1 <691388500@qq.com>
2025-10-15 14:24:01 +0800

    读取txt中的加训图片数据，在数据集中删除其他

---
+++ reserve_train_images_dataset.py
@@ -0,0 +1,23 @@
+"""
+1、从txt文档中按行读取要叠加的数据
+2、数据集创建测试集新的版本（复制）
+3、在数据集中保留读取的图片数据，删除其他
+"""
+from lizard_class.dataSet import DataSet
+from data.read_txt import read_txt
+
+url = "http://10.110.152.15:9011/"
+# 数据集名称
+dataset_name = "10-14倍耐力数据"
+# 数据集版本
+dataset_version = "6"
+# 从文件（add_train_images）中读取的数据
+ng_images = read_txt("./data/add_train_images.txt")
+# 测试集中所有图片数据
+dataset = DataSet(url, dataset_name, dataset_version)
+dataset_version_images = dataset.get_dataset_images()
+# 仅保留文件中的图片数据，其余操作删除
+for key, value in dataset_version_images.items():
+    if key not in ng_images:
+        dataset.delete_images(str(value))
+print(f"已找到需要叠加ad数据的图片数据{len(ng_images)}张！")
