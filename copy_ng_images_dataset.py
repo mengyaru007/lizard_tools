4092825f41f658c02794f59c2cf11a553509b57f
mengyr1 <691388500@qq.com>
2025-10-16 14:55:36 +0800

    数据集复制图片

---
+++ copy_ng_images_dataset.py
@@ -0,0 +1,22 @@
+"""
+同一数据集下从不同版本中复制NG图片
+"""
+from lizard_class.dataSet import DataSet
+from data.read_txt import read_txt
+
+url = "http://10.110.152.15:9011/"
+# 数据集名称
+dataset_name = "10-14倍耐力数据"
+# 数据集目标版本
+dataset_target_version = "12"
+# 从文件中读取的数据
+ng_images = read_txt("./data/export_ng_images.txt")
+# 来源版本
+score_version = ["1", "2", "3", "4"]
+for version in score_version:
+    dataset = DataSet(url, dataset_name, version)
+    dataset_version_images = dataset.get_dataset_images()
+    # 仅保留文件中的图片数据，其余操作删除
+    for value in ng_images:
+        if value in dataset_version_images.keys():
+            dataset.copy_images(dataset_version_images[value], dataset_target_version)
