c22a49df904891514cdedc58d25ca4d93eea6aa3
mengyr1 <691388500@qq.com>
2025-10-22 18:18:51 +0800

    同一数据集下从不同版本中复制NG图片：get_dataset_images修改返回格式{id：name}

--- copy_ng_images_dataset.py
+++ copy_ng_images_dataset.py
@@ -4,19 +4,18 @@
 from lizard_class.dataSet import DataSet
 from data.read_txt import read_txt
 
-url = "http://10.110.152.15:9011/"
+url = "http://10.110.156.90:9012/"
 # 数据集名称
-dataset_name = "10-14倍耐力数据"
+dataset_name = "10-17倍耐力数据"
 # 数据集目标版本
-dataset_target_version = "12"
+dataset_target_version = "2"
 # 从文件中读取的数据
 ng_images = read_txt("./data/export_ng_images.txt")
 # 来源版本
-score_version = ["1", "2", "3", "4"]
+score_version = ["1"]
 for version in score_version:
     dataset = DataSet(url, dataset_name, version)
     dataset_version_images = dataset.get_dataset_images()
-    # 仅保留文件中的图片数据，其余操作删除
     for value in ng_images:
-        if value in dataset_version_images.keys():
+        if value in dataset_version_images.values():
             dataset.copy_images(dataset_version_images[value], dataset_target_version)
