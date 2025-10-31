4da007cf8ca0438668a63cf1837c399e60c92c29
mengyr1 <691388500@qq.com>
2025-10-31 11:02:11 +0800

    判定节点

---
+++ lizard_class/decideNode.py
@@ -0,0 +1,24 @@
+from lizard_class.taskItem import TaskItem
+
+class DecideNode(TaskItem):
+
+    def __init__(self, host, model_name, task_name):
+        super().__init__(host, model_name, task_name)
+        self.decide_node_id = 0
+        self.get_decide_node_id()
+
+    def get_decide_node_id(self):
+        """获取判定节点id"""
+        url = self.host + "/api/v1/projects/" + str(self.project_id) + "/tasks/" + str(self.task_id) + "/task-config"
+        print(f"请求接口{url}")
+        res = self.session.request("get", url).json()
+        print(f"{url}的响应数据：{res}")
+        self.decide_node_id = res['result']['decideNodeId']
+
+    def get_execute_result(self):
+        """获取判定结果评估结果"""
+        url = self.host + "/api/v1/projects/" + str(self.project_id) + "/tasks/" + str(self.task_id) + f"/task-nodes/{self.decide_node_id}/execute-result"
+        print(f"请求接口{url}")
+        res = self.session.request("get", url).json()
+        print(f"{url}的响应数据：{res}")
+        return res['result']['list']
