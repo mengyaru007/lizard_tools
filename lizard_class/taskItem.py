ec53e82ad16d7e987c10d876b5be76808deb035c
mengyr1 <691388500@qq.com>
2026-01-28 16:44:59 +0800

    检测项接口请求

--- lizard_class/taskItem.py
+++ lizard_class/taskItem.py
@@ -9,6 +9,7 @@ class TaskItem(EnvCon):
         self.project_id = 0
         self.task_id = 0
         self.node_id = 0
+        self.node_key = ""
         self.get_project_id()
         self.get_task_id()
 
@@ -36,26 +37,28 @@ class TaskItem(EnvCon):
                 self.task_id = item['taskId']
                 break
 
-    def get_node_id(self, version=0, node_name="异常检测"):
-        """获取节点版本id"""
+    def get_node_id(self, node_name="异常检测"):
+        """获取节点版本配置"""
         url = self.host + "/api/v1/projects/" + str(self.project_id) + "/tasks/" + str(self.task_id) + "/pipeline"
         print(f"请求接口{url}")
         res = self.session.request("get", url).json()
         print(f"{url}的响应数据：{res}")
         task_list = res['result']['nodes']
-        node_key = ""
         for item in task_list:
             if item['name'] == node_name:
-                node_key = item['key']
-                if node_name == "分类":
-                    self.node_id = item['id']
-                break
-        if node_name == "异常检测":
-            url = self.host + "/api/v1/projects/" + str(self.project_id) + "/tasks/" + str(self.task_id) + "/task-nodes/" + node_key + "/versions?pageSize=9999&pageNum=1&onlyVersion=true"
-            print(f"请求接口{url}")
-            res = self.session.request("get", url).json()
-            print(f"{url}的响应数据：{res}")
-            version_list = res['result']['list']
-            for item in version_list:
-                if item['currentVersion'] == version:
-                    self.node_id = item['nodeId']
+                self.node_key = item['key']
+                self.node_id = item['id']
+                return
+
+    def get_ad_node_id(self, ad_node_name, version):
+        """获取ad节点版本id"""
+        self.get_node_id(ad_node_name)
+        url = self.host + "/api/v1/projects/" + str(self.project_id) + "/tasks/" + str(
+            self.task_id) + "/task-nodes/" + self.node_key + "/versions?pageSize=9999&pageNum=1&onlyVersion=true"
+        print(f"请求接口{url}")
+        res = self.session.request("get", url).json()
+        print(f"{url}的响应数据：{res}")
+        version_list = res['result']['list']
+        for item in version_list:
+            if item['currentVersion'] == version:
+                self.node_id = item['nodeId']
