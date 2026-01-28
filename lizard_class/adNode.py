4c265ada7aa5f4d1b9eacfaf493f81ad3ff8f760
mengyr1 <691388500@qq.com>
2026-01-28 16:33:48 +0800

    AD节点接口请求

--- lizard_class/adNode.py
+++ lizard_class/adNode.py
@@ -2,9 +2,27 @@ from lizard_class.taskItem import TaskItem
 
 class ADNode(TaskItem):
 
-    def get_normal_pictures(self, version, score_min, score_max):
+    def get_ad_node_config(self, node_name="异常检测"):
+        """获取节点配置"""
+        self.get_node_id(node_name)
+        url = self.host + "/api/v1/projects/" + str(self.project_id) + "/tasks/" + str(self.task_id) + "/task-nodes/" + str(self.node_id) + "/config"
+        print(f"请求接口{url}")
+        res = self.session.request("get", url).json()
+        print(f"{url}的响应数据：{res}")
+        return res["result"]["params"]
+
+    def get_ad_training_images(self, version = 1):
+        """获取ad节点的训练数据"""
+        dataset_id = self.get_ad_node_config()["datasetId"]
+        url = self.host + "/api/v1/datasets/" + str(dataset_id) + "/versions/" + str(version) + "/samples"
+        print(f"请求接口{url}")
+        res = self.session.request("get", url).json()
+        print(f"{url}的响应数据：{res}")
+        return res["result"]
+
+    def get_ad_normal_pictures(self, node_name, version, score_min, score_max):
         """获取ok图片列表"""
-        self.get_node_id(version)
+        self.get_ad_node_id(node_name, version)
         url = self.host + "/api/v1/projects/" + str(self.project_id) + "/tasks/" + str(self.task_id) + "/task-nodes/" + str(self.node_id) + "/execute-result?type=normal"
         print(f"请求接口{url}")
         res = self.session.request("get", url).json()
@@ -17,9 +35,9 @@ class ADNode(TaskItem):
         print(f"OK图片总共{len(images_name_list)}个图片！")
         return images_name_list
 
-    def get_abnormal_pictures(self, version, score_min, score_max):
+    def get_ad_abnormal_pictures(self, node_name, version, score_min, score_max):
         """获取ng图片列表"""
-        self.get_node_id(version)
+        self.get_ad_node_id(node_name, version)
         url = self.host + "/api/v1/projects/" + str(self.project_id) + "/tasks/" + str(self.task_id) + "/task-nodes/" + str(self.node_id) + "/execute-result?type=abnormal"
         print(f"请求接口{url}")
         res = self.session.request("get", url).json()
